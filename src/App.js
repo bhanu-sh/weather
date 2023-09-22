1660459ab125ac28997b8e446b7b15438e7f5d16
Bhanu Sharma <bhanu1234sharma@gmail.com>
2023-09-22 20:46:53 +0530

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
+
+  // const url = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=804c2a8dd514de4512e2e0af79ef81f6';
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes >
+          <Route element = {<Home />} path="/" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,100 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const Home = () => {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=804c2a8dd514de4512e2e0af79ef81f6`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(url)
+        .then((res) => {
+          setData(res.data);
+          setError("");
+          console.log(res, data);
+        })
+        .catch((err) => {
+          if (err.response && err.response.status === 404) {
+            setError("City not found");
+          } else {
+            setError("An error occurred");
+          }
+          console.log(err);
+        });
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="my-5">
+        <h1 className="text-center">Weather App</h1>
+        <div className="mx-5 text-center">
+          <input
+            className="form-control form-control-lg shadow"
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            onKeyDown={searchLocation}
+            type="text"
+            placeholder="Enter City Name"
+          />
+          {error && <p className="error">{error}</p>}
+          <form>
+            <button
+              type="submit"
+              className=" shadow btn btn-primary m-3 btn-lg"
+            >
+              Search
+            </button>
+          </form>
+          
+        </div>
+
+        {data.main ? (
+          <>
+            <div
+              className="card weather text-white shadow p-3 row"
+              style={{
+                backgroundImage: `url("/weathers/${data.weather[0].main}.jpg"`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+              }}
+            >
+               
+              <div className="main my-2 col-md-4">
+                <div className="p-5 transparent">
+                  <h1>{data.name}</h1>
+                  <h2>Temp: {Math.round(data.main.temp)}° C</h2>
+                  <img
+                    src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                    width={50}
+                    alt=""
+                  />
+                  <h4>{data.weather[0].main}</h4>
+                </div>
+              </div>
+              {/* <div className="main my-2 col-md-4">
+                <div className="p-5 transparent">
+                  <h1>{data.name}</h1>
+                  <h2>Temp: {Math.round(data.main.temp)}° C</h2>
+                  <img
+                    src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                    width={50}
+                    alt=""
+                  />
+                  <h4>{data.weather[0].main}</h4>
+                </div>
+              </div> */}
+            </div>
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
