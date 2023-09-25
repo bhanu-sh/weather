c652c5f3bf439e054a587d15c06a8d765f7d6f68
Bhanu Sharma <bhanu1234sharma@gmail.com>
2023-09-25 19:37:28 +0530

    extension fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
   const [error, setError] = useState("");
+  const [weather, setWeather] = useState(`Clear`)
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=804c2a8dd514de4512e2e0af79ef81f6`;
 
@@ -15,6 +16,7 @@ const Home = () => {
         .then((res) => {
           setData(res.data);
           setError("");
+          setWeather(res.data.weather[0].main);
           console.log(res, data);
         })
         .catch((err) => {
@@ -29,12 +31,20 @@ const Home = () => {
   };
 
   return (
-    <div className="container">
-      <div className="my-5">
+    <div
+      className="vh-100"
+      style={{
+        backgroundImage: `url("/weathers/${weather}.jpg"`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="container py-4">
         <h1 className="text-center">Weather App</h1>
-        <div className="mx-5 text-center">
+        <div className="mx-5 text-center my-3">
           <input
-            className="form-control form-control-lg shadow"
+            className="form-control form-control-lg shadow my-2"
             value={location}
             onChange={(event) => setLocation(event.target.value)}
             onKeyDown={searchLocation}
@@ -50,45 +60,27 @@ const Home = () => {
               Search
             </button>
           </form>
-          
         </div>
 
         {data.main ? (
           <>
-            <div
-              className="card weather text-white shadow p-3 row"
-              style={{
-                backgroundImage: `url("/weathers/${data.weather[0].main}.jpg"`,
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-              }}
-            >
-               
-              <div className="main my-2 col-md-4">
-                <div className="p-5 transparent">
-                  <h1>{data.name}</h1>
-                  <h2>Temp: {Math.round(data.main.temp)}° C</h2>
-                  <img
-                    src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-                    width={50}
-                    alt=""
-                  />
-                  <h4>{data.weather[0].main}</h4>
-                </div>
+            <div className="main mx-5 my-2 row">
+              <div className="p-5 transparent col-md-6">
+                <h1>{data.name}</h1>
+                <h2>Temp: {Math.round(data.main.temp)}° C</h2>
+                <img
+                  src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                  width={50}
+                  alt=""
+                />
+                <h4>{data.weather[0].main}</h4>
               </div>
-              {/* <div className="main my-2 col-md-4">
-                <div className="p-5 transparent">
-                  <h1>{data.name}</h1>
-                  <h2>Temp: {Math.round(data.main.temp)}° C</h2>
-                  <img
-                    src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-                    width={50}
-                    alt=""
-                  />
-                  <h4>{data.weather[0].main}</h4>
-                </div>
-              </div> */}
+              <div className="p-5 transparent col-md-6">
+                <h2>Humidity: {data.main.humidity}%</h2>
+                <h2>Visibility: {Math.round(data.visibility / 1000)} KM</h2>
+                <h3>Wind:</h3>
+                <p>Speed: {Math.round(data.wind.speed)} m/sec</p>
+              </div>
             </div>
           </>
         ) : null}
