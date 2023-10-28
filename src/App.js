6b367e938bc5fc76274dc4837c5113c957fd5a90
Bhanu Sharma <bhanu1234sharma@gmail.com>
2023-10-28 21:08:07 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-
   // const url = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=804c2a8dd514de4512e2e0af79ef81f6';
-
 
   return (
     <div>
       <BrowserRouter>
-        <Routes >
-          <Route element = {<Home />} path="/" />
+        <Toaster position="top-center" reverseOrder={false} />
+        <Routes>
+          <Route element={<Home />} path="/" />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const Home = () => {
   const [data, setData] = useState({});
@@ -11,27 +12,30 @@ const Home = () => {
 
   const searchLocation = (event) => {
     event.preventDefault();
+
     axios
       .get(url)
       .then((res) => {
         setData(res.data);
         setError("");
         setWeather(res.data.weather[0].main);
-        console.log(res, data);
+        console.log(res.data);
       })
       .catch((err) => {
+        console.error("An error occurred:", err);
         if (err.response && err.response.status === 404) {
           setError("City not found");
+          toast.error(error);
         } else {
           setError("An error occurred");
+          toast.error(error);
         }
-        console.log(err);
       });
   };
 
   return (
     <div
-      className="vh-100"
+      className="main-body"
       style={{
         backgroundImage: `url("/weathers/${weather}.jpg"`,
         backgroundRepeat: "no-repeat",
@@ -43,19 +47,16 @@ const Home = () => {
         <h1 className="text-center">Weather App</h1>
         <div className="mx-5 text-center my-3">
           <form onSubmit={searchLocation}>
-            <div class="blur shadow input-group mb-5">
+            <div className="blur shadow input-group mb-5">
               <input
                 type="text"
-                value={location}
-                onChange={(event) => setLocation(event.target.value)}
+                name="location"
                 className="form-control mx-auto form-control-lg"
                 placeholder="Enter City Name"
+                value={location} // Bind input value to the location state
+                onChange={(e) => setLocation(e.target.value)} // Update the location state when input changes
               />
-              <button
-                class="btn btn-primary btn-lg"
-                type="button"
-                id="button-addon1"
-              >
+              <button className="btn btn-primary btn-lg" type="submit" id="button-addon1">
                 Search
               </button>
             </div>
@@ -65,25 +66,22 @@ const Home = () => {
         {data.main ? (
           <>
             <div className="main mx-5 my-2 row">
-              <div className="p-5 transparent col-md-6">
+              <div className="p-5 transparent weather col-md-6">
                 <h1>{data.name}</h1>
                 <h2>Temp: {Math.round(data.main.temp)}° C</h2>
-                <img
-                  src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-                  width={50}
-                  alt=""
-                />
+                <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} width={50} alt="" />
                 <h4>{data.weather[0].main}</h4>
               </div>
               <div className="p-5 transparent col-md-6">
                 <h2>Humidity: {data.main.humidity}%</h2>
                 <h2>Visibility: {Math.round(data.visibility / 1000)} KM</h2>
-                <div className="card wind">
+                <div className="border-top border-3 wind">
                   <h3>Wind:</h3>
                   <p>Speed: {Math.round(data.wind.speed)} m/sec</p>
                 </div>
               </div>
             </div>
+            
           </>
         ) : null}
       </div>
